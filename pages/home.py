f8cc475b7eea90b5dd508ed22734aacf3db4b139
JoePelz <joe.pelz@gmail.com>
2016-08-02 14:40:39 -0700

    added home js/css

--- pages/home.py
+++ pages/home.py
@@ -6,7 +6,9 @@ class Home:
 
     # handle HTTP GET requests here.  Name gets value from routing rules above.
     def GET(self):
-        return str(common.render._head(self.pageTitle)) \
+        return str(common.render._head(self.pageTitle,
+                                       stylesheets=["/static/css/home.css"],
+                                       scripts=["/static/js/home.js"])) \
              + str(common.render._header(common.navbar, self.pageTitle)) \
              + str(common.render.home(self.pageTitle)) \
              + str(common.render._tail())
