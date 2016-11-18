af7779e1c8833a69780c08cecae130552494c78e
JoePelz <joe.pelz@gmail.com>
2016-11-18 13:27:11 -0800

    added images to home page

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
-    // lazy load images
-    image = document.getElementById("screen");
-    image.addEventListener('click', function() {
-        $('.ui.modal').modal('show');
+
+    // enable image popups
+    var tabs = document.getElementById("tabs");
+    var images = tabs.getElementsByTagName("IMG");
+    var i = images.length - 1;
+    while (i >= 0) {
+        images[i].onclick = function () {
+            $('.ui.image.modal').modal('show');
+        };
+        i -= 1;
+    }
+
+    //make modal closeable
+    var modal = document.getElementById("imageviewer");
+    modal.addEventListener('click', function() {
+        $('.ui.image.modal').modal('hide');
     });
-    image = document.getElementById("modal");
-    image.addEventListener('click', function() {
-        $('.ui.modal').modal('hide');
+
+    //activate tabs
+    $('.menu .item').tab({
+        onLoad: function (tabName) {
+            var img = document.getElementById("scr_" + tabName);
+            var mi = document.getElementById("modal_image");
+            mi.src = img.src;
+        }
     });
 });
